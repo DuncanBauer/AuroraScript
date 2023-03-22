4c16d0af8bcb6246e768610b00dcd61d5bad5808
Duncan <duncanebauer@gmail.com>
2023-03-22 02:09:57 -0700

    init commit

---
+++ premake5.lua
@@ -0,0 +1,90 @@
+workspace "Tests"
+	architecture "x64"
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+	
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include dirs relative to root folder
+IncludeDir = {}
+IncludeDir["AuroraScript"] = "AuroraScript/src"
+
+project "AuroraScript"
+	location "AuroraScript"
+	kind "StaticLib"
+	staticruntime "on"
+	language "C++"
+	cppdialect "C++20"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/src/**.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		defines
+		{
+			"_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+		
+project "AuroraScriptTesting"
+	location "AuroraScriptTesting"
+	kind "ConsoleApp"
+	staticruntime "on"
+	language "C++"
+	cppdialect "C++20"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/src/**.h"
+	}
+
+	includedirs
+	{
+		"%{IncludeDir.AuroraScript}"
+	}
+
+	links
+	{
+		"AuroraScript"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		defines
+		{
+			"_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "_RELEASE"
+		runtime "Release"
+		optimize "on"
